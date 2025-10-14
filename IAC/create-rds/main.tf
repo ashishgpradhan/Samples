6bc56107407fd8e2c8e798432e1e46fbe3f5e56a
ashishgpradhan <ashish.g.pradhan@gmail.com>
2025-10-14 10:24:07 +0100

    creates rds instance

---
+++ IAC/create-rds/main.tf
@@ -0,0 +1,22 @@
+resource "aws_db_instance" "rdsinstance" {
+  allocated_storage = 20
+  storage_type      = "gp2"
+  engine            = "mysql"
+  engine_version    = "8.0"
+  instance_class    = "db.t3.micro"
+
+  parameter_group_name   = "default.mysql8.0"
+  skip_final_snapshot    = true
+  publicly_accessible    = true
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.default.name
+}
+
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [data.aws_subnets.default_subnet.ids[0], data.aws_subnets.default_subnet.ids[1]]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
